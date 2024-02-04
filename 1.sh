2fc6cad71ba7fb57931250fe9a3f4215c666d33e
buhenxihuan <32200913+buhenxihuan@users.noreply.github.com>
2024-02-04 17:22:42 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,17 @@
-rm -rf disk.img
+# rm -rf disk.img
+# dd if=/dev/zero of=disk.img bs=3M count=24
+# mkfs.ext4  ./disk.img
+# mkdir -p mnt
+# sudo mount disk.img mnt
+# # 根据命令行参数生成对应的测例
+# sudo cp -r ./testcases/$1/* ./mnt/
+# sudo umount mnt
+# rm -rf mnt
+# sudo chmod 777 disk.img
+
+rm disk.img
 dd if=/dev/zero of=disk.img bs=3M count=24
-mkfs.ext4  ./disk.img
+mkfs.vfat -F 32 disk.img
 mkdir -p mnt
 sudo mount disk.img mnt
 # 根据命令行参数生成对应的测例
@@ -8,4 +19,3 @@ sudo cp -r ./testcases/$1/* ./mnt/
 sudo umount mnt
 rm -rf mnt
 sudo chmod 777 disk.img
-
