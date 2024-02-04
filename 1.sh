7d4cd2ede3da725d720b90ee4569b9e6c5acac38
buhenxihuan <32200913+buhenxihuan@users.noreply.github.com>
2024-02-04 17:26:00 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -9,9 +9,9 @@
 # rm -rf mnt
 # sudo chmod 777 disk.img
 
-rm disk.img
+rm -rf disk.img
 dd if=/dev/zero of=disk.img bs=3M count=24
-mkfs.vfat -F 32 disk.img
+mkfs.vfat -F 32 ./disk.img
 mkdir -p mnt
 sudo mount disk.img mnt
 # 根据命令行参数生成对应的测例
